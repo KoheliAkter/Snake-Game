f8ab50151095621fddab879deff4be3c89c1112f
Koheli Akter <koheliakter344@gmail.com>
2024-05-08 21:45:49 +0600

    jhkj

--- Snake.py
+++ Snake.py
@@ -1,1 +1,18 @@
 from turtle import *
+import turtle
+import time
+import random
+
+delay = 0.1
+score = 0
+high_score = 0
+
+# Creating a window screen
+wn = turtle.Screen()
+wn.title("Snake Game")
+wn.bgcolor("blue")
+# the width and height can be put as user's choice
+wn.setup(width=600, height=600)
+wn.tracer(0)
+
+done()
