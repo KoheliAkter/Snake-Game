30555f46bea4a1bf98d865841b1e864f2ff14f84
Koheli Akter <koheliakter344@gmail.com>
2024-05-09 23:17:05 +0600

    working on snakehade

--- Snake.py
+++ Snake.py
@@ -7,12 +7,34 @@ delay = 0.1
 score = 0
 high_score = 0
 
+
 # Creating a window screen
 wn = turtle.Screen()
 wn.title("Snake Game")
 wn.bgcolor("blue")
 # the width and height can be put as user's choice
 wn.setup(width=600, height=600)
-wn.tracer(10)
+wn.tracer(0)
+
+
+# head of the snake
+
+head = turtle.Turtle()
+head.shape("square")
+head.color("white")
+head.penup()
+head.goto(0, 0)
+head.direction = "Stop"
+
+
+
+
+
+
+
+
+
+
+
 
 done()
