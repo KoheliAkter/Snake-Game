a169aca49ba883019ed7347a7adf57b9c93f4678
Koheli Akter <koheliakter344@gmail.com>
2024-05-13 16:01:06 +0600

    working on snake move

--- Snake.py
+++ Snake.py
@@ -1,4 +1,4 @@
-from turtle import *
+
 import turtle
 import time
 import random
@@ -14,27 +14,87 @@ wn.title("Snake Game")
 wn.bgcolor("blue")
 # the width and height can be put as user's choice
 wn.setup(width=600, height=600)
-wn.tracer(0)
+# wn.tracer(0)
 
 
 # head of the snake
 
 head = turtle.Turtle()
+head.speed(0)
 head.shape("square")
 head.color("white")
 head.penup()
-head.goto(0, 0)
-head.direction = "Stop"
+head.goto(0,0)
+head.direction = "stop"
+
+# food in the game 
+
+food = turtle.Turtle()
+colors = random.choice(['red','green','black'])
+shapes = random.choice(['square','triangle','circle'])
+
+food.speed(0)
+food.shape(shapes)
+food.color(colors)
+food.penup()
+food.goto(0,100)
+
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape('square')
+pen.color('white')
+pen.penup()
+pen.hideturtle()
+pen.goto(0,250)
+pen.write('Score : 0 High Score : 0', align='center',
+          font=('candara',24,'bold'))
+
+
+# assigning key directions
+
+def group():
+	if head.direction != "down":
+		head.direction = "up"
+
+
+def godown():
+	if head.direction != "up":
+		head.direction = "down"
+
+
+def goleft():
+	if head.direction != "right":
+		head.direction = "left"
+
+
+def goright():
+	if head.direction != "left":
+		head.direction = "right"
+
+
+def move():
+	if head.direction == "up":
+		y = head.ycor()
+		head.sety(y+20)
+	if head.direction == "down":
+		y = head.ycor()
+		head.sety(y-20)
+	if head.direction == "left":
+		x = head.xcor()
+		head.setx(x-20)
+	if head.direction == "right":
+		x = head.xcor()
+		head.setx(x+20)
+
+wn.listen()
+wn.onkeypress(group,'w')
+wn.onkeypress(godown,'s')
+wn.onkeypress(goleft,'a')
+wn.onkeypress(goright,'d')
 
 
 
 
+wn.mainloop()
 
-
-
-
-
-
-
-
-done()
+#done()
